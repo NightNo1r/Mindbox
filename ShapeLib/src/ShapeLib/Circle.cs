914bec3763d5158b210fd1d9333ada1637e3a5cc
Artem <57496218+NightNo1r@users.noreply.github.com>
2024-04-18 19:44:20 +0300

    Add ShapeLib Project

---
+++ ShapeLib/src/ShapeLib/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ShapeLib.Abstractions;
+
+namespace ShapeLib
+{
+    public class Circle : IShape
+    {
+        private double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
